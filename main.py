9514924ada2c777ef8592b47eddb849798a085b8
Carina <88159599+MaybeCarina@users.noreply.github.com>
2022-04-30 20:36:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,73 +1,117 @@
-import random 
+public class MonteCarlo {
 
-def MyBernoulli(p):
-    U = random.random();
-    X = 0
-    if U < p:
-        X = 1
-    else:
-        X = 0
+    public static void main(String[] args) {
+        int trials = 0;
+        int totalDays = 0;
+        int totalInfected = 0;
 
-    return X
+        while (trials < 150000) {
+            int infected = 1;
+            int days = 1;
 
-n = 20 # number of computers in the network
-r = 5 # max number of computers cleaned daily double
-p = 0.1 # probability of spreading the virus
+            int n = 20; // number of computers in the network
+            int r = 5; // max number of computers cleaned daily
+            double p = 0.1; // probability of spreading the virus
 
-trials = 1
-totalDays = 0
-comps = [n]
-tempInfectedCount = 1
+            // Initially computer #0 is infected (set to 1),
+            // computers #1 through #19 are clean (set to 0).
+            int[] comps = new int[n];
+            comps[0] = 1;
+            for (int i = 1; i < n; i++) {
+                comps[i] = 0;
+            }
 
-while trials < 10001:
+            // For each wire "infected -> non-infected":
+            // perform a Bernoulli trial with probability p=0.1;
+            // if success, computer #j becomes newly_infected (set to 2).
 
-    infectedCount = 1;
-    days = 0
-    comps.append(1)
-    # Initially computer  # 0 is infected (set to 1),
-    # computers # 1 thru #19 are clean (set to 0).
-    for i in range(1, n):
-        comps.append(0)
-    infectedComps = []
-    while infectedCount != 0:
-        days = days+1
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if ((comps[i] == 1) && (comps[j] == 0)) {
+                        int x = MyBernoulli(p);
+                        if (x == 1) {
+                            comps[j] = 2;
 
-    # For each wire "infected -> noninfected":
-    # perform a Bernoulli trial with probability p=0.1
-    # if success, computer #j becomes newly infected (set to 2).
-        for i in range(n):
-            for j in range(n):
-                if comps[i] == 1 and comps[j] == 0:
-                    x = MyBernoulli(p)
-                    if (x == 1):
-                        comps[j] = 2
-                        infectedCount = infectedCount+1
-                        infectedComps.append(i)
+                        }
+                    }
+                }
+            }
 
-    # After that, mark all newly infected as simply infected.
-        for i in range(n):
-            if comps[i] == 2:
-                comps[i] = 1
-                tempInfectedCount = tempInfectedCount+1
-    #clean
-        if infectedCount <= r:
-            infectedCount = 0
-            for i in range(n):
-                comps[i] = 0
-        else:
-            for i in range(r):
-                if len(infectedComps) > 0:
-                    num = random.random() 
-                    len(infectedComps)
-                    index = infectedComps.get(num)
-                    comps[index] = 0
-                    infectedComps.remove(num)
-                    infectedCount = infectedCount-1
+            // After that, mark all newly infected as simply infected.
+            for (int i = 0; i < n; i++) {
+                if (comps[i] == 2) {
+                    comps[i] = 1;
+                    infected++;
+                }
+            }
+            totalInfected += infected;
+
+            //while loop to count days until virus removed from system
+            while (infected != 0) {
+
+                //clean - remove 5 infected
+                int count = 1;
+                for (int i = 0; i < n; i++) {
+                    if (count <= r) {
+                        if (comps[i] == 1) {
+                            comps[i] = 0;
+                            count++;
+                            infected--;
+                        }
+                    }
+                }
+
+                int temp = 0;
+                for (int j = 0; j < n; j++) {
+                    if (comps[j] == 1) {
+                        temp++;
+                    }
+                }
+                if (temp > 0) {
+                    days++;
+                    for (int i = 0; i < n; i++) {
+                        for (int j = 0; j < n; j++) {
+                            if ((comps[i] == 1) && (comps[j] == 0)) {
+                                int x = MyBernoulli(p);
+                                if (x == 1) {
+                                    comps[j] = 2;
+                                }
+                            }
+                        }
+                    }
+                    for (int i = 0; i < n; i++) {
+                        if (comps[i] == 2) {
+                            comps[i] = 1;
+                            infected++;
+                        }
+                    }
+                }
+
+            }
+            totalDays += days;
+            trials++;
+        }
+        double avgInfected = (double) totalInfected / (trials);
+        double probInfected = (double) totalInfected / (20*trials);
+        double remInfected = (double) totalDays / trials;
+        System.out.println("Average days to remove from system: " + remInfected);
+        System.out.println("Probability that each computer gets infected: " + probInfected);
+        System.out.println("Expected number of computers that get infected: " + avgInfected);
+    }
 
 
-    totalDays = totalDays + days
-    trials = trials+1
+    public static int MyBernoulli(double p) {
+        double U = Math.random();
+        int X;
+        if (U < p) {
+            X = 1;
+        }
+        else {
+            X = 0;
+        }
+        return X;
+    }
 
-days = totalDays / (trials - 1)
-compsNum = tempInfectedCount / (trials - 1)
+
+}
 
